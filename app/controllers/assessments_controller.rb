bce457f1d3225187273a389a89bccce037c98e54
Jeff Powers <jeffp2007@gmail.com>
2015-08-22 18:45:57 +0000

    Make submit button create submit assessment

--- app/controllers/assessments_controller.rb
+++ app/controllers/assessments_controller.rb
@@ -6,4 +6,16 @@ class AssessmentsController < ApplicationController
 	def new
 		@assessment = Assessment.new
 	end
+
+	def create
+		Assessment.create(assessment_params)
+		redirect_to root_path
+	end
+
+	private
+
+	def assessment_params
+		params.require(:assessment).permit(:question1, :question2, :question3, :question4, :question5, :question6, :question7, :question8, :question9, :question 10)
+	end
+
 end
