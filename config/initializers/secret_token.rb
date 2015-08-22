4098f6fabda4456310847619b99efa82f9eb7d66
Jeff Powers <jeffp2007@gmail.com>
2015-08-22 16:29:02 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Tcfgrader::Application.config.secret_key_base = '53db6457c81424438414b75f866be8e846ee22a91764d985f19b13aa5bd5e1fa2a6cc7c32523ca2537e98eaee6b09a5bb956c9e2b84039d78969adc688b0877f'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Tcfgrader::Application.config.session_store :cookie_store, key: '_tcfgrader_session'
