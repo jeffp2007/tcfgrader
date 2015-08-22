4da894da57790b75730fc4c48c0d6e83d0c88216
Jeff Powers <jeffp2007@gmail.com>
2015-08-22 18:20:15 +0000

    Added user authentication with devise

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,4 +1,5 @@
 Tcfgrader::Application.configure do
+  config.action_mailer.default_url_options = { :host => 'tcfgrader.herokuapp.com' }
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
