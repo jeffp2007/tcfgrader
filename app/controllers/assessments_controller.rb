069b2b0a887b27786cd6e98df850ef74b1fab55b
Jeff Powers <jeffp2007@gmail.com>
2015-08-22 18:42:26 +0000

    Add form to take assessment

--- app/controllers/assessments_controller.rb
+++ app/controllers/assessments_controller.rb
@@ -2,4 +2,8 @@ class AssessmentsController < ApplicationController
 	def index
 		@assessments = Assessment.all 
 	end
+
+	def new
+		@assessment = Assessment.new
+	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Tcfgrader::Application.routes.draw do
   devise_for :users
   root 'assessments#index'
+  resources :assessments
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
