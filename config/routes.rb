ce8c3b8155cafc43cc65e2f429af78cfd01ee285
Jeff Powers <jeffp2007@gmail.com>
2015-08-22 16:38:35 +0000

    Set up the home page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Tcfgrader::Application.routes.draw do
+  root 'assessments#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
