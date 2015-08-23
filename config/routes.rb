4d588698372da01b0b0438feca153e1447f8fec1
Jeff Powers <jeffp2007@gmail.com>
2015-08-23 02:29:57 +0000

    Create user profile page

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Tcfgrader::Application.routes.draw do
   devise_for :users
   root 'assessments#index'
   resources :assessments
+  resources :users, :only => :show
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
